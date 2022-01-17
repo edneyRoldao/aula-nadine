b5284a90108114b4e6f80beafc0020afe29d744f
edneyRoldao <edneyroldao@gmail.com>
2022-01-17 19:09:15 -0300

    more code

--- lista-exercicios-final/05-ordenacao-des.js
+++ lista-exercicios-final/05-ordenacao-des.js
@@ -1,3 +1,7 @@
 const despesas = require('./base/lista-despesas')();
 
+
 // ordenar de forma crescente(DES) a lista de despesas pela descricao
+
+
+// ordenar de forma crescente(DES) a lista de despesas pela descricao com sort da api de collection
